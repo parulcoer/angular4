f94296590654bf0493a546262b51275a51e1c514
Parul Sharma <parul.coer@gmail.com>
2018-02-22 21:47:27 -0500

    RxJs Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
+  /*getDishes(): Promise<Dish[]> {
     return new Promise(resolve=> {
       // Simulate server latency with 2 second delay
         setTimeout(() => resolve(DISHES), 2000);
@@ -26,5 +31,28 @@ export class DishService {
       // Simulate server latency with 2 second delay
         setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
     });
+  }*/
+  /*observable to Promise
+  getDishes(): Promise<Dish[]> {
+    return Observable.of(DISHES).delay(2000).toPromise();
+  }
+
+  getDish(id: number): Promise<Dish> {
+    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000).toPromise();
+  }
+
+  getFeaturedDish(): Promise<Dish> {
+    return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000).toPromise();
+  }*/
+  getDishes(): Observable<Dish[]> {
+    return Observable.of(DISHES).delay(2000);
+  }
+
+  getDish(id: number): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
+  }
+
+  getFeaturedDish(): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class LeaderService {
 
   constructor() { }
-  getLeaders(): Promise<Leader[]>{
+  /*getLeaders(): Promise<Leader[]>{
     return new Promise(resolve=> {
       setTimeout(() => resolve(LEADERS),2000);
     });
@@ -22,6 +27,33 @@ export class LeaderService {
     return new Promise(resolve=> {
       setTimeout(() => resolve(LEADERS.filter((leader) => (leader.featured))[0]) , 2000);
     });
+  }
+  observable to promise
+  getLeaders(): Promise<Leader[]>{
+    return Observable.of(LEADERS).delay(2000).toPromise();
+    
+  }
+
+  getLeader(id: Number): Promise<Leader>{
+    return Observable.of(LEADERS.filter(( leader ) => ( leader.id === id ))[0]).delay(2000).toPromise();
+    
+  }
+
+  getFeaturedLeader(): Promise<Leader>{
+    return Observable.of(LEADERS.filter((leader) => (leader.featured))[0]).delay(2000).toPromise();
+  }*/
+  getLeaders(): Observable<Leader[]>{
+    return Observable.of(LEADERS).delay(2000);
+    
+  }
+
+  getLeader(id: Number): Observable<Leader>{
+    return Observable.of(LEADERS.filter(( leader ) => ( leader.id === id ))[0]).delay(2000);
+    
+  }
+
+  getFeaturedLeader(): Observable<Leader>{
+    return Observable.of(LEADERS.filter((leader) => (leader.featured))[0]).delay(2000);
   }
 
 }
